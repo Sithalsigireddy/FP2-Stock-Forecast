9d57e87585b4e835c2ccd534c48b05a6bc67b2cc
Sithalsigireddy <sithalrao@gmail.com>
2024-07-11 20:37:07 +0530

    region chnages and csv added

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ import streamlit as st
 import boto3
 import pandas as pd
 import io
- 
-# Define the SageMaker endpoint names and regions
+
+# Load AWS credentials from Streamlit secrets
+aws_access_key_id = st.secrets["aws_access_key_id"]
+aws_secret_access_key = st.secrets["aws_secret_access_key"]
+aws_region = st.secrets["region"]
+
+# Define the SageMaker endpoint names
 SHORT_TERM_ENDPOINT_NAME = "canvas-shortterm"  # Short-term endpoint name
 LONG_TERM_ENDPOINT_NAME = "canvas-new-deployment-07-11-2024-2-00-AM"  # Long-term endpoint name
-AWS_REGION = "eu-north-1"  # Specify your AWS region
- 
+
 # Local path where the downloaded file is saved
 local_file_path = 'newprocessed_TATAMOTORS.NS_stock_data.csv'
- 
+
 # Function to call the SageMaker endpoint
-def get_forecast_from_sagemaker(data, endpoint_name, aws_region):
-    client = boto3.client("runtime.sagemaker", region_name=aws_region)
+def get_forecast_from_sagemaker(data, endpoint_name, aws_region, aws_access_key_id, aws_secret_access_key):
+    client = boto3.client(
+        "runtime.sagemaker", 
+        region_name=aws_region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
     body = data.to_csv(index=False).encode("utf-8")
     try:
         response = client.invoke_endpoint(
@@ -32,37 +41,37 @@ def get_forecast_from_sagemaker(data, endpoint_name, aws_region):
     except Exception as e:
         st.error(f"Error invoking endpoint: {e}")
         return None
- 
+
 # Streamlit interface
 st.title("Forecast Tata Motors Stock")
- 
+
 # Information about forecast types
 st.info("Select 'Short Term' for a 7-day forecast or 'Long Term' for a 365-day forecast.")
- 
+
 # Dropdown for forecast type
 forecast_type = st.selectbox("Select Forecast Type", ["Short Term", "Long Term"])
- 
+
 # Load the historical data from the local file
 historical_df = pd.read_csv(local_file_path)
- 
+
 # Prepare the data for prediction
 def prepare_data_for_prediction(historical_data):
     # Assume the future dates and other necessary columns will be added by the model
     return historical_data
- 
+
 # Prepare combined data
 combined_df = prepare_data_for_prediction(historical_df)
- 
+
 # Select the appropriate endpoint
 if forecast_type == "Short Term":
     endpoint_name = SHORT_TERM_ENDPOINT_NAME
 else:
     endpoint_name = LONG_TERM_ENDPOINT_NAME
- 
+
 # Display a button to trigger the forecast
 if st.button("Get Forecast"):
     # Get the forecast
-    forecast_df = get_forecast_from_sagemaker(combined_df, endpoint_name, AWS_REGION)
+    forecast_df = get_forecast_from_sagemaker(combined_df, endpoint_name, aws_region, aws_access_key_id, aws_secret_access_key)
     if forecast_df is not None:
         if forecast_type == "Long Term":
             # Restrict long-term predictions to 365 days
